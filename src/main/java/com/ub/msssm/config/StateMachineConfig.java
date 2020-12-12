ed62450c335d8777c022c77318933e660f261c51
Kartik <kartik15nov@gmail.com>
2020-12-12 22:08:43 +0530

    Experimenting with Spring State Machine State change Listener.

--- src/main/java/com/ub/msssm/config/StateMachineConfig.java
+++ src/main/java/com/ub/msssm/config/StateMachineConfig.java
@@ -6,8 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListener;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
 
@@ -34,5 +38,18 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                 .withExternal().source(PaymentStates.NEW).target(PaymentStates.PRE_AUTH).event(PaymentEvents.PRE_AUTH_APPROVED)
                 .and()
                 .withExternal().source(PaymentStates.NEW).target(PaymentStates.AUTH_ERROR).event(PaymentEvents.PRE_AUTH_DECLINED);
+    }
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<PaymentStates, PaymentEvents> config) throws Exception {
+        StateMachineListener<PaymentStates, PaymentEvents> adapter = new StateMachineListenerAdapter<>() {
+            @Override
+            public void stateChanged(State<PaymentStates, PaymentEvents> from, State<PaymentStates, PaymentEvents> to) {
+                log.info("State Changed from: {}, to: {}", from, to);
+            }
+        };
+
+        config.withConfiguration()
+                .listener(adapter);
     }
 }
